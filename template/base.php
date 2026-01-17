a98acb938f25e050c3164e51ad4622978aae8584
ravag <alerava4@gmail.com>
2026-01-17 18:59:52 +0100

    fixed foreach cycle

--- template/base.php
+++ template/base.php
@@ -53,11 +53,12 @@
         <h2>I TUOI GRUPPI</h2>
         <?php foreach($templateParams["Gruppi"] as $gruppo): ?>
             <div class="gruppo">
-                <h3><?php echo $gruppo['nomeGruppo']; ?></h3>
-                <p>Descrizione: <?php echo $gruppo['descrizione']; ?></p>
-                <p>Categoria: <?php echo $gruppo['categoria']; ?></p>
+                <h3><?php echo $gruppo['nome']; ?></h3>
+                <p>Descrizione: <?php echo $gruppo['descr_breve']; ?> </p>
+                <p>Categoria: <?php echo $gruppo['corso_di_riferimento']; ?> </p>
                 <button>Entra nel gruppo</button>
             </div>
+        <?php endforeach; ?>
     </main>
     <footer>
         <p>Daniele Tramonti (daniele.tramonti2@studio.unibo.it) - Alessandro Ravaioli (alessandro.ravaioli8@studio.unibo.it) - Marco Bulgarelli (marco.bulgarelli6@studio.unibo.it)</p>
