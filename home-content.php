c94f93a36efb0654ed1dfa7af277087bc08c2376
Marco Bulgarelli <marco.bulgarelli6@studio.unibo.it>
2026-01-20 16:05:29 +0100

    improved overall css

--- home-content.php
+++ home-content.php
@@ -1,4 +1,4 @@
 <article>
-    <h2>titolo</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet repellendus hic voluptates aliquid, explicabo necessitatibus praesentium quibusdam sed facilis reprehenderit omnis doloremque quisquam iste minima assumenda quae commodi labore delectus.</p>
+    <h2>Titolo</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet repellendus hic voluptates aliquid, explicabo necessitatibus praesentium quibusdam sed facilis reprehenderit omnis doloremque quisquam iste minima assumenda quae commodi labore delectus.
 </article>
