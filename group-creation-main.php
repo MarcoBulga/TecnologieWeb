d941f3ad5dd91ddcecd63b42e81e485f41bf5bcc
ravag <alerava4@gmail.com>
2026-01-20 10:48:20 +0100

    made all elements functional in group creation page

--- group-creation-main.php
+++ group-creation-main.php
@@ -3,18 +3,29 @@
         <ul>
             <li><label for="course">Seleziona corso : <select name="course" id="course" required>
                 <option value="" disabled selected hidden>seleziona corso</option>
-                <option value="Tecnologie Web">Tecnologie Web</option>
+                <?php foreach($templateParams["courses"] as $course): ?>
+                <option value="<?php echo $course["nome"] ?>"><?php echo $course["nome"] ?></option>
+                <?php endforeach; ?>
             </select></label></li>
             <li><label for="groupname">Inserire nome gruppo : <input type="text" name="groupname" id="groupname" required /></label></li>
             <li><label for="groupsize">Numero partecipanti : <input type="number" id="groupsize" name="groupsize" value="1" min="1" max="6" required /></label></li>
             <li>
                 <p>Tag del gruppo : </p><br/>
-                <button id="tagbutton" name="tagbutton">Scegli tag</button>
+                <button type="button" id="tag-button" name="tag-button">Scegli tag</button>
             </li>
             <li><label for="private">Privato <input type="checkbox" name="private" id="private" /></label></li>
             <li><label for="shortdescription">Descrizione breve : </label><br/><textarea name="shortdescription" id="shortdescription" required></textarea></li>
             <li><label for="longdescription">Descrizione estesa : </label><br/><textarea name="longdescription" id="longdescription" required></textarea></li>
         </ul>
+        <div class="filters">
+            <ul>
+                <?php foreach($templateParams["filters"] as $filter): 
+                    $safeId = str_replace(" ", "_", $filter["nome"]);
+                    ?>
+                <li><label for="<?php echo $safeId; ?>"><input type="checkbox" id="<?php echo $safeId; ?>" name="<?php echo $safeId; ?>"><?php echo $safeId; ?></label></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
         <input type="reset" id="undobutton" name="undobutton" value="Annulla" />
         <input type="submit" id="confirmbutton" name="confirmbutton" value="Conferma" />
         <?php
