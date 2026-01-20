b1e689569d2188940dc5af746fa313af154139d8
Marco Bulgarelli <marco.bulgarelli6@studio.unibo.it>
2026-01-20 11:58:27 +0100

    login page is verified

--- home-content.php
+++ home-content.php
@@ -1,3 +1,4 @@
 <article>
+    <h2>titolo</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet repellendus hic voluptates aliquid, explicabo necessitatibus praesentium quibusdam sed facilis reprehenderit omnis doloremque quisquam iste minima assumenda quae commodi labore delectus.</p>
 </article>
